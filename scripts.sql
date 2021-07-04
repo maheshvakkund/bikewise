a4cc0507c7af6a116ddd1bcf94603c724a6255ef
maheshvakkund <maheshvakkund96@gmail.com>
2021-07-05 00:51:10 +0530

    Initial Commit : Scripts for Database and Tables

---
+++ scripts.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS `bikewise_db`;
+CREATE TABLE `bikewise_data` (
+  `id` bigint NOT NULL,
+  `title` text,
+  `description` text,
+  `address` text,
+  `occurred_at` bigint DEFAULT NULL,
+  `updated_at` bigint DEFAULT NULL,
+  `url` text,
+  `location_type` text,
+  `location_description` text,
+  `type` text,
+  `type_properties` text,
+  `source_name` text,
+  `source_html_url` text,
+  `source_api_url` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
